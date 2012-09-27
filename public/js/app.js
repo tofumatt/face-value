c7efde6dd1473e00f5ea22550720c827d807168b
Matthew Riley MacPherson <matt@lonelyvegan.com>
2012-09-27 17:50:39 -0400

    Cleanup app.js code

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-/*global EJS:true */
 'use strict';
 
 // The code below uses require.js, a module system for javscript:
@@ -8,33 +7,33 @@ require.config({
     baseUrl: 'js',
     paths: {
         'ejs': ['lib/ejs'],
-        'jquery': ['lib/jquery'],
-        'install': ['lib/install']
+        'jquery': ['lib/jquery']
     },
 })
 
-// America, fuck yeah!
-var DEFAULT_CURRENCIES = '#USD,EUR'
-
+// We make these globals so EJS can use them like helpers.
+// TODO: Make this better.
 var getCurrencies
 var getPluralName
 var valueFormatted
 
 // When you write javascript in separate files, list them as
 // dependencies along with jquery
-define("app", function(require) {
+define('app', function(require) {
+    // America, fuck yeah!
+    var DEFAULT_CURRENCIES = '#USD,EUR'
 
-    // if (window.location.pathname !== '/app') {
-    //    return
-    // }
-
+    // TODO: Don't use jQuery? Maybe something more lightweight, or just
+    // straight-up JS?
     var $ = require('jquery')
     require('ejs')
     var EJS = window.EJS
     var ls = window.localStorage
 
-    // START HERE: Put your js code here
+    // Run this code as soon as the document is ready.
     function init() {
+        // TODO: Replace this with a version number and some way to do JS
+        // "upgrades".
         if (ls.ranInit !== '1') {
             firstRun()
         } else {
@@ -52,6 +51,8 @@ define("app", function(require) {
     }
 
     function firstRun() {
+        // Initialize localStorage variables so we don't get undefined/type
+        // errors.
         ls.currentCurrencies = '[]'
         ls.currencies = '{}'
         ls.ranInit = '1'
@@ -111,7 +112,7 @@ define("app", function(require) {
     }
 
     function renderHeader() {
-    	$('#header').attr('class', getCurrencies()[0] + '-flag')
+        $('#header').attr('class', getCurrencies()[0] + '-flag')
         $('#header').html(new EJS({url: '/views/header.ejs'}).render({}))
     }
 
@@ -133,71 +134,46 @@ define("app", function(require) {
     }
 
     function setupListeners() {
-        $(function() {
-            $(window).on('hashchange', function(event) {
-                updateCurrencies()
-                renderCurrencies()
-                renderHeader()
-                renderLists()
-                // renderSwitcher()
-            })
-
-            $('#first-currency').live('click', function(event) {
-                bodyScrolling(false)
-                $('#first-select').removeClass('hide')
-                event.preventDefault()
-
-                return false
-            })
-
-            $('#second-currency').live('click', function(event) {
-                bodyScrolling(false)
-                $('#second-select').removeClass('hide')
-                event.preventDefault()
-
-                return false
-            })
-
-            $('#first-select,#second-select').on('blur', function(event) {
-                disablePopover()
-            })
-
-            $('body').on('click', function(event) {
-                disablePopover()
-            })
-
-            $('body').on('touchmove', function(event) {
-                $('a').removeClass('active')
-            })
-
-            $('a').live('touchstart', function(event) {
-                $(this).addClass('active')
-            })
-
-            $('a').live('touchcancel,touchend,touchmove', function(event) {
-                $(this).removeClass('active')
-            })
-
-            // $('#first-select,#second-select').on('change', function(event) {
-            //     $('#first-select,#second-select').removeClass('show')
-            //     $('#first-currency,#second-currency').trigger('blur')
-            //     window.location.hash = event.target.value
-            // })
+        $(window).on('hashchange', function(event) {
+            updateCurrencies()
+            renderCurrencies()
+            renderHeader()
+            renderLists()
         })
 
-        // $('#select-first-currency').on('click', function(event) {
-        //     $('#select-first-currency').popover({
-        //         content: $('#currency-list-first').html(),
-        //         placement: 'bottom',
-        //         trigger: 'manual'
-        //     })
-        //     $('#select-first-currency').popover('toggle')
-        //     event.preventDefault()
-        //     return false
-        // })
-        // $('#select-first-currency').popover({
-        //     content: $('#currency-list-first').html()
-        // })
+        $('#first-currency').live('click', function(event) {
+            bodyScrolling(false)
+            $('#first-select').removeClass('hide')
+
+            return false
+        })
+
+        $('#second-currency').live('click', function(event) {
+            bodyScrolling(false)
+            $('#second-select').removeClass('hide')
+
+            return false
+        })
+
+        $('#first-select,#second-select').on('blur', function(event) {
+            disablePopover()
+        })
+
+        $('body').on('click', function(event) {
+            disablePopover()
+        })
+
+        $('body').on('touchmove', function(event) {
+            $('a').removeClass('active')
+        })
+
+        $('a').live('touchstart', function(event) {
+            $(this).addClass('active')
+        })
+
+        $('a').live('touchcancel,touchend,touchmove', function(event) {
+            $(this).removeClass('active')
+        })
     }
 
     function updateCurrencies() {
@@ -231,55 +207,8 @@ define("app", function(require) {
         return amount
     }
 
-    init()
-
-    // Hook up the installation button, feel free to customize how
-    // this works
-    
-    // var install = require('install');
-
-    // function updateInstallButton() {
-    //     $(function() {
-    //         var btn = $('.install-btn');
-    //         if(install.state == 'uninstalled') {
-    //             btn.show();
-    //         }
-    //         else if(install.state == 'installed' || install.state == 'unsupported') {
-    //             btn.hide();
-    //         }
-    //     });
-    // }
-
-    // $(function() {
-    //     $('.install-btn').click(install);        
-    // });
-
-    // install.on('change', updateInstallButton);
-
-    // install.on('error', function(e, err) {
-    //     // Feel free to customize this
-    //     $('.install-error').text(err.toString()).show();
-    // });
-
-    // install.on('showiOSInstall', function() {
-    //     // Feel free to customize this
-    //     var msg = $('.install-ios-msg');
-    //     msg.show();
-        
-    //     setTimeout(function() {
-    //         msg.hide();
-    //     }, 8000);
-    // });
-});
-
-// Include the in-app payments API, and if it fails to load handle it
-// gracefully.
-// https://developer.mozilla.org/en/Apps/In-app_payments
-// require(['https://marketplace-cdn.addons.mozilla.net/mozmarket.js'],
-//         function() {},
-//         function(err) {
-//             window.mozmarket = window.mozmarket || {};
-//             window.mozmarket.buy = function() {
-//                 alert('The in-app purchasing is currently unavailable.');
-//             };
-//         });
+    // Let's do it!
+    $(function() {
+        init()
+    })
+})

518c05a4fda2f220a7f5478073da01a3ef130107
Matthew Riley MacPherson <matt@lonelyvegan.com>
2012-09-28 04:09:28 -0400

    Add line break in header text (close #22)

---
+++ test/front-end/test.styles.js
@@ -0,0 +1,23 @@
+/*global $:true, casper:true, document:true */
+'use strict';
+
+casper.test.comment('Face Value - Styles')
+
+casper.start('http://localhost:3001/', function () {
+  casper.test.info('Testing styles')
+})
+
+casper.waitForSelector('#currency-selector', function() {
+  this.test.assertEval(function() {
+    return $('#currency-selector').height() === 38
+  }, 'Currency selector should be 38px tall by default')
+
+  this.test.assertEval(function() {
+    $('#currency-selector').html('')
+    return $('#currency-selector').height() === 38
+  }, 'Currency selector should be 38px tall with no content')
+})
+
+casper.run(function () {
+  this.test.done()
+})

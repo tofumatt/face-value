b07f00e24e777dad7fb99ba09658848bc37c9a8f
Matthew Riley MacPherson <matt@lonelyvegan.com>
2012-10-02 16:37:28 -0400

    Fix retina CSS queries

--- script/generate-flag-css.js
+++ script/generate-flag-css.js
@@ -7,7 +7,7 @@ var denominations = JSON.parse(fs.readFileSync('./lib/denominations.json', 'utf8
 
 for (var i in denominations) {
   if (denominations.hasOwnProperty(i)) {
-    console.log('.{code}-flag {\n    background-image: url(/img/flags/{code}.png);\n}\n\n#header.{code}-flag {\n    background-image: url(/img/header-flags/{code}.png);\n}\n@media screen and (-webkit-min-device-pixel-ratio: 2), screen and (max--moz-device-pixel-ratio: 2) {#header.{code}-flag {\n    background-image: url(/img/header-flags/{code}@2x.png);\n}}\n'.format({
+    console.log('.{code}-flag {\n    background-image: url(/img/flags/{code}.png);\n}\n\n#header.{code}-flag {\n    background-image: url(/img/header-flags/{code}.png);\n}\n@media screen and (-webkit-min-device-pixel-ratio: 2), screen and (min--moz-device-pixel-ratio: 2), (min-resolution: 2dppx) {#header.{code}-flag {\n    background-image: url(/img/header-flags/{code}@2x.png);\n}}\n'.format({
         code: i
     }))
   }
